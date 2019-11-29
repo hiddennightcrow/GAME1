7aca68842b3f44ff98cfc4f3b7a73e0d8a18f598
hiddennightcrow <57255290+hiddennightcrow@users.noreply.github.com>
2019-11-29 15:46:34 +0800

    進度1

---
+++ Assets/Female 01-2.tsx
@@ -0,0 +1,11 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.1" name="Female 01-2" tilewidth="32" tileheight="32" tilecount="12" columns="3">
+ <image source="PIPOYA FREE RPG Character Sprites 32x32/Female/Female 01-2.png" width="96" height="128"/>
+ <tile id="0">
+  <animation>
+   <frame tileid="0" duration="150"/>
+   <frame tileid="1" duration="150"/>
+   <frame tileid="2" duration="150"/>
+  </animation>
+ </tile>
+</tileset>
---
+++ Assets/Female.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Female : MonoBehaviour
+{
+    
+    public float movespeed=5f;
+    public Rigidbody2D rb;
+
+}
