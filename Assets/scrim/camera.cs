38ba8c922ebc8c320e678e1a7f6b6e9727c65280
hiddennightcrow <57255290+hiddennightcrow@users.noreply.github.com>
2019-12-15 15:48:26 +0800

    進度8

---
+++ Assets/scrim/camera.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camera : MonoBehaviour
+{
+    [Header("跟隨目標")]
+    public Transform target;
+    //[Header("跟隨速度"), Range(0f, 100f)]
+    //public float speed = 2f;
+
+    /// <summary>
+    /// 跟隨目標功能
+    /// </summary>
+    private void Track()
+    {
+       
+
+        // 三維向量 目標座標 = 新 三維向量(目標.座標.X，限制後的 Y，-10) - 攝影機 Z 預設為 -10
+        Vector3 targetPos = new Vector3(target.position.x, target.position.y, -10);
+
+        // 攝影機.座標 = 三維向量.插值(攝影機.座標，目標座標，百分比 * 速度 * 一幀的時間)
+        transform.position = Vector3.Lerp(transform.position, targetPos, Time.deltaTime);
+    }
+
+    // 在 Update 執行後才執行 LateUpdate，適合用於跟隨
+    private void LateUpdate()
+    {
+        Track();
+    }
+}
---
+++ Assets/scrim/player.cs
@@ -0,0 +1,24 @@
+﻿
+using UnityEngine;
+
+public class player : MonoBehaviour
+{
+    
+    public float moveSpeed = 0.01f;
+    public Rigidbody2D rb;
+    public Animator animator;
+    Vector2 movement;
+     void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        animator.SetFloat("Horizontal", movement.x);
+        animator.SetFloat("Vertical", movement.y);
+        animator.SetFloat("speed", movement.sqrMagnitude);
+    }
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * moveSpeed);
+    }
+}
